fad9cd6c897fee91588e47914ba55b3e26f967ee
Matheus Felipe <matheus.reirasi@gmail.com>
2022-10-14 00:19:14 -0300

    nave do jogador e tiros

--- components.py
+++ components.py
@@ -1,52 +1,61 @@
 import pygame
 
-class Spaceship_1(pygame.sprite.Sprite):
+import globals as glb
+
+class Spaceship(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
 
 
-        self.image = pygame.transform.scale(pygame.image.load('spaceship.png'), (30, 40))
-        self.rect = self.image.get_rect()
+        self.spaceship_image = pygame.transform.scale(pygame.image.load('./assets/Spaceships/01/Spaceship_01_BLUE.png'), (50, 60))
+        self.rect = self.spaceship_image.get_rect()
         self.x = x
         self.y = y
-        self.vel_x = 10
-        self.vel_y = 10
 
-    def move_spaceship(self, userInput):
-        if userInput[pygame.K_d] and self.image.get_width() + self.x < pygame.display.get_window_size()[0]:
-            self.x += self.vel_x
-        if userInput[pygame.K_a] and self.x > 0:
-            self.x -= self.vel_x
-        if userInput[pygame.K_w] and self.y > 0:
-            self.y -= self.vel_y
-        if userInput[pygame.K_s] and self.image.get_height() + self.y < pygame.display.get_window_size()[1]:
-            self.y += self.vel_y
+        self.bullet_group = pygame.sprite.Group()        
+        self.last_shot = pygame.time.get_ticks()
 
-    def draw(self, win):
-        win.blit(self.image, (self.x, self.y))
+    def move_spaceship(self):
+        self.userInput = pygame.key.get_pressed()
+
+        if self.userInput[pygame.K_d] and self.spaceship_image.get_width() + self.x < pygame.display.get_window_size()[0]:
+            self.x += glb.SPACESHIP_SPEED
+        if self.userInput[pygame.K_a] and self.x > 0:
+            self.x -= glb.SPACESHIP_SPEED
+        if self.userInput[pygame.K_w] and self.y > 0:
+            self.y -= glb.SPACESHIP_SPEED
+        if self.userInput[pygame.K_s] and self.spaceship_image.get_height() + self.y < pygame.display.get_window_size()[1]:
+            self.y += glb.SPACESHIP_SPEED
+    
+
+    def shot_player(self, window_game):
+        self.time_now = pygame.time.get_ticks()
+        if (self.userInput[pygame.K_SPACE] and self.time_now - self.last_shot > self.cooldown):
+            bullet = Bullets(self.x+15, self.y-35)
+            self.bullet_group.add(bullet)
+            bullet.update(window_game)
+            self.last_shot = self.time_now
+            print("shoot")
 
 
-class Spaceship_2(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def update(self, window_game):
+        self.cooldown = 500 #milissegundos
+        self.move_spaceship()
+        self.shot_player(window_game)
+        window_game.blit(self.spaceship_image, (self.x, self.y))
+        self.bullet_group.update(window_game)
+        
+
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self,x,y):
         pygame.sprite.Sprite.__init__(self)
-
-
-        self.image = pygame.transform.scale(pygame.image.load('assets/Graphics-Asset/Spaceships/01/Spaceship_01_BLUE.png'), (60, 70))
-        self.rect = self.image.get_rect()
+        self.bullet_image = pygame.transform.scale(pygame.image.load("./assets/bullet.png"),(20,45))
+        self.rect = self.bullet_image.get_rect()  
         self.x = x
         self.y = y
-        self.vel_x = 10
-        self.vel_y = 10
 
-    def move_spaceship(self, userInput):
-        if userInput[pygame.K_RIGHT] and self.image.get_width() + self.x < pygame.display.get_window_size()[0]:
-            self.x += self.vel_x
-        if userInput[pygame.K_LEFT] and self.x > 0:
-            self.x -= self.vel_x
-        if userInput[pygame.K_UP] and self.y > 0:
-            self.y -= self.vel_y
-        if userInput[pygame.K_DOWN] and self.image.get_height() + self.y < pygame.display.get_window_size()[1]:
-            self.y += self.vel_y
-
-    def draw(self, win):
-        win.blit(self.image, (self.x, self.y))
+    def update(self, window_game):
+        self.y -= glb.BULLET_SPEED
+        if self.rect.bottom < 0:
+            self.kill()
+        window_game.blit(self.bullet_image, (self.x, self.y))
