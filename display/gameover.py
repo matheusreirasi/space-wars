ffbc9fe75f7bdac6225b3faffca4707a173f1a58
Matheus Felipe <matheus.reirasi@gmail.com>
2022-12-14 23:01:10 -0300

    a pontuação ainda não aparece corretamente

--- display/gameover.py
+++ display/gameover.py
@@ -2,25 +2,27 @@ import pygame, sys
 import globals as glb
 
 from display import play
+from components import Player
 
 class GameOver(object):
     def __init__(self):
         self.scroll_page = 0
-        cooldown = 0
-        cooldown_limit = 0
 
         self.window_game = pygame.display.set_mode((glb.GAME_WIDTH, glb.GAME_HEIGHT))
+        self.gameover_image = pygame.transform.scale(pygame.image.load('./assets/images/gameover.png'), (glb.GAME_WIDTH, glb.GAME_HEIGHT))
 
-        self.calvou_font = pygame.font.Font("assets/font.ttf", 200)
+        self.calvou_font = pygame.font.Font("assets/font.ttf", 150)
         self.score_font = pygame.font.Font("assets/font.ttf", 30)
 
-        self.gameover_image = pygame.transform.scale(pygame.image.load('./Sprites/Background/gameover.png'), (glb.GAME_WIDTH, glb.GAME_HEIGHT))
 
         self.score = play.Player(0,0).score
+        #self.score = play.Play().player.score
+        #self.score = Player(0,0).score
+
 
 
     def increase_scroll(self):
-        self.scroll_page -= 4.5
+        self.scroll_page -= 3.5
         if abs(self.scroll_page) > glb.GAME_WIDTH:
             self.scroll_page = 0
 
@@ -28,7 +30,6 @@ class GameOver(object):
 
     
     def update(self):
-        self.window_game.fill(glb.GAME_BACKGROUND_COLOR)
 
 
         for i in range(0,2):
@@ -38,10 +39,10 @@ class GameOver(object):
 
 
         self.calvou_text = self.calvou_font.render("CALVOU", True, "white")
-        self.window_game.blit(self.calvou_text, (glb.GAME_WIDTH/2, glb.GAME_HEIGHT/4))
+        self.window_game.blit(self.calvou_text, (glb.GAME_WIDTH/2 - self.calvou_text.get_width()/2, glb.GAME_HEIGHT/5))
 
         self.score_text = self.score_font.render("Score: " + str(self.score), True, "white")
-        self.window_game.blit(self.score_text, (glb.GAME_WIDTH/2, glb.GAME_HEIGHT/3))
+        self.window_game.blit(self.score_text, (glb.GAME_WIDTH/2 - self.score_text.get_width()/2, 2*glb.GAME_HEIGHT/3))
 
 
 
